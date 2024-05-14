d19d9783fb06ac0a88a9397e6d0d2ff3995b2093
anismumtaz <anismumtaz126@gmail.com>
2024-05-14 16:53:08 +0100

    Add files via upload

--- modelling/modelling.py
+++ modelling/modelling.py
@@ -1,15 +1,25 @@
 from model.randomforest import RandomForest
-
-
+from model.svm import SVM
+from model.gnb import GNB
 
 def model_predict(data, df, name):
-    results = []
+    
     print("RandomForest")
     model = RandomForest("RandomForest", data.get_embeddings(), data.get_type())
     model.train(data)
     model.predict(data.X_test)
     model.print_results(data)
 
-
-def model_evaluate(model, data):
+    print("SVM")
+    model = SVM("SVM", data.get_embeddings(), data.get_type())
+    model.train(data)
+    model.predict(data.X_test)
     model.print_results(data)
+
+    print("GNB")
+    model = GNB("GNB", data.get_embeddings(), data.get_type())
+    model.train(data)
+    model.predict(data.X_test)
+    model.print_results(data)
+
+
